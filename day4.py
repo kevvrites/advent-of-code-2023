24b36f97b3a07d851e966eb04366a902c74f52df
Kevin Liu <liukproductivity@gmail.com>
2023-12-10 11:57:51 -0500

    completed day4 part 2

--- day4.py
+++ day4.py
@@ -22,23 +22,21 @@ print(total_points)
 
 # Part 2
 
-cards_to_process = lines.copy()
-original_lines = lines.copy()  # Keep a copy of the original lines for reference
+cards = []
+for line in lines:
+    parts = re.split(r'\||:', line)
+    winning_numbers = set(map(int, parts[1].strip().split()))
+    drawn_numbers = set(map(int, parts[2].strip().split()))
+    cards.append((winning_numbers, drawn_numbers))
+
+card_counts = {i: 1 for i in range(len(cards))}
 total_cards = 0
 
-while cards_to_process:
-    line = cards_to_process.pop(0)  # Process the next card in the queue
-    card_numbers = re.split(r'\||:', line)[1:]
-    winning_numbers = set(int(num) for num in card_numbers[0].strip().split())
-    drawn_numbers = set(int(num) for num in card_numbers[1].strip().split())
-    matches = len(winning_numbers & drawn_numbers)
+for i, (winning, drawn) in enumerate(cards):
+    matches = len(winning & drawn)
+    total_cards += card_counts[i]
 
-    total_cards += 1  # Add the current card to the total count
-
-    # Add the corresponding number of subsequent cards to the queue
-    card_index = original_lines.index(line)
-    for i in range(1, matches + 1):
-        if card_index + i < len(original_lines):
-            cards_to_process.append(original_lines[card_index + i])
+    for j in range(i + 1, min(i + 1 + matches, len(cards))):
+        card_counts[j] += card_counts[i]
 
 print(total_cards)
